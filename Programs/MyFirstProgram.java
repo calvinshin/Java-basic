ff0a49e39973a4b53b91228b8b56dadbb56ac4e5
Calvin Shin <calvinshin@Calvins-MacBook-Pro.local>
2020-04-01 19:38:16 -0700

    Hello World add

---
+++ Programs/MyFirstProgram.java
@@ -0,0 +1,8 @@
+// This is my MyFirstProgram.java Java program.
+public class MyFirstProgram
+{
+           public static void main(String[] args)
+           {
+              System.out.println("Hello World!");
+           }
+        }
